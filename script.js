f4a535d81e4817d12fb0d616cdf339da0aab638c
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-04-20 14:42:30 -0500

    feat: POST movies complete

--- script.js
+++ script.js
@@ -5,14 +5,60 @@ $(window).on("load", function() {
 });
 
 $(document).ready(function() {
-    $.ajax("https://elemental-sepia-strawflower.glitch.me/movies")
-        .done(function(movies) {
+    $.ajax("https://pollen-impossible-bangle.glitch.me/movies")
+        .done(function(movies){
+            displayMovies(movies)
+        })
+        .fail(function() {
+          $(".card").html("Oops, something went wrong :(");
+        });
 
-            console.log(movies);
-            for (let i = 0; i < movies.length; i++) {
-                if  (movies[i].title !== undefined && movies[i].rating !== undefined)  {
-                    console.log(movies[i].title);
-                    $(".card-deck").append(`
+    $('#submit-movie-rating').click(function (e){
+        e.preventDefault();
+       let userInputTitle = $('#Movie-Input-Title').val()
+        let userInputRating = $('#Movie-Input-Rating').val()
+        let userInput = {title: userInputTitle, rating: userInputRating}
+        console.log(userInput);
+        fetch(`https://pollen-impossible-bangle.glitch.me/movies`,{
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userInput)
+        }).then(function (response){
+            return response.json()
+            console.log("The movie you enter was :"+response);
+        }).then(function (results){
+            console.log(results);
+            displayMovies(results)
+        })
+    })
+
+});
+
+
+// function deleteMovie(id){
+//   fetch(`https://elemental-sepia-strawflower.glitch.me/${id}`,{
+//     method: "DELETE"
+//   })
+// }
+
+// //we will need movie title, description, etc. from the user
+
+
+
+// //need to get the options that the user wants to change for that movie
+// function editMovie(id){
+
+// }
+
+// //render all of the movies to html
+function displayMovies(movies){
+    console.log(movies);
+    for (let i = 0; i < movies.length; i++) {
+        if  (movies[i].title !== undefined && movies[i].rating !== undefined)  {
+            console.log(movies[i].title);
+            $(".card-deck").append(`
                         <div class="card">
                             <div class="card-body">
                                 <h5 class="card-title">${movies[i].title}</h5>
@@ -26,53 +72,7 @@ $(document).ready(function() {
                                 <li class="list-group-item"><em>RATING:</em> ${movies[i].rating}</li>
                             </ul>
                         </div>`)
-                }
+        }
 
-            }
-            // movies.forEach(function(movie) {
-            //     console.log(movie.year);
-            //     $(".card-deck").append(`
-            //         <div class="card">
-            //             <div class="card-body">
-            //                 <h5 class="card-title">${movie.title}</h5>
-            //                 <p class="card-text">${movie.plot}</p>
-            //             </div>
-            //             <ul class="list-group list-group-flush">
-            //                 <li class="list-group-item year">${movie.year}</li>
-            //                 <li class="list-group-item genre">${movie.genre}</li>
-            //                 <li class="list-group-item director">${movie.director}</li>
-            //                 <li class="list-group-item actors">${movie.actors}</li>
-            //                 <li class="list-group-item rating">${movie.rating}</li>
-            //             </ul>
-            //         </div>`)
-            // })
-        })
-        .fail(function() {
-          $(".card").html("Oops, something went wrong :(");
-        });
-});
-
-// function updateMovies(movies){
-
-// }
-
-// function deleteMovie(id){
-//   fetch(`https://elemental-sepia-strawflower.glitch.me/${id}`,{
-//     method: "DELETE"
-//   })
-// }
-
-// //we will need movie title, description, etc. from the user
-// function addMovie(){
-
-// }
-
-// //need to get the options that the user wants to change for that movie
-// function editMovie(id){
-
-// }
-
-// //render all of the movies to html
-// function displayMovies(movies){
-  
-// }
+    }
+}
